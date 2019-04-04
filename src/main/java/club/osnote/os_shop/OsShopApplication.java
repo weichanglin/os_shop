fdd0a07481d1634a4788ea9653fb0c64b34569d9
wcl <weichanglin001@126.com>
2019-04-04 17:03:17 +0800

    init

---
+++ src/main/java/club/osnote/os_shop/OsShopApplication.java
@@ -0,0 +1,14 @@
+package club.osnote.os_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class OsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/club/osnote/os_shop/controller/MainController.java
@@ -0,0 +1,13 @@
+package club.osnote.os_shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
