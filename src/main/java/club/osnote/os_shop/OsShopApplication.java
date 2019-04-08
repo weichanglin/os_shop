0292cdbb3ee212be75ea3059213508dd224ab24e
wcl <weichanglin001@126.com>
2019-04-08 11:00:17 +0800

    logger配置

--- src/main/java/club/osnote/os_shop/OsShopApplication.java
+++ src/main/java/club/osnote/os_shop/OsShopApplication.java
@@ -1,14 +1,23 @@
 package club.osnote.os_shop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OsShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsShopApplication.class, args);
+
+	}
+
+	@Bean
+	public Logger logger(){
+		return LoggerFactory.getLogger(OsShopApplication.class);
 	}
 
 }
--- src/main/java/club/osnote/os_shop/controller/MainController.java
+++ src/main/java/club/osnote/os_shop/controller/MainController.java
@@ -1,6 +1,7 @@
 package club.osnote.os_shop.controller;
 
 import club.osnote.os_shop.service.TestService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,13 @@ public class MainController {
 
     @Autowired
     TestService testService;
+    @Autowired
+    Logger logger;
 
     @ResponseBody
     @RequestMapping("/")
     public String index(){
+        logger.info("hello,测试日志=======");
         return testService.sayhello("zhang");
     }
 }
